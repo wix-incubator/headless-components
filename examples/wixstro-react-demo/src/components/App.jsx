ac4a884f2e8d797f096c8849b1323e76ac0da9c3
Nitay Neeman <nitayneeman@users.noreply.github.com>
2025-03-23 08:44:33 +0200

    migrate code

--- examples/wixstro-react-demo/src/components/App.jsx
+++ examples/wixstro-react-demo/src/components/App.jsx
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo.src} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/components/App.jsx</code> and save to reload.
         </p>
         <a
           className="App-link"
