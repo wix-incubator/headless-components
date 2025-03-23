d5691c6e3ae9e6dfa114eb9814d59c1a5d84e865
Nitay Neeman <nitayneeman@users.noreply.github.com>
2025-03-23 10:29:58 +0200

    add test

---
+++ examples/wixstro-react-demo/playwright.config.js
@@ -0,0 +1,26 @@
+// @ts-check
+import { defineConfig, devices } from "@playwright/test";
+
+export default defineConfig({
+  testDir: "./tests",
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: "list",
+
+  use: {
+    baseURL: "https://astro-react-demo-yql4lfsc-org-headless7.wix-sites.dev",
+    trace: "on-first-retry",
+    timeout: 30 * 1000,
+    expect: { timeout: 5000 },
+    navigationTimeout: 15 * 1000,
+  },
+
+  projects: [
+    {
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
+    },
+  ],
+});
