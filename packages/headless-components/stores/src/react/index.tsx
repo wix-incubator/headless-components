99df155fd6f1f19fc5dc07ebb6cb59b2977f2658
Yury Michurin <yurym@wix.com>
2025-05-26 16:04:11 +0300

    Wroking buy now

--- packages/headless-components/stores/src/react/index.tsx
+++ packages/headless-components/stores/src/react/index.tsx
@@ -1,5 +1,5 @@
 import { cart } from "@wix/ecom";
-import { redirects } from "@wix/redirects";
+import { redirects, checkout } from "@wix/redirects";
 import { useState } from "react";
 
 export function BuyNow(props: {
@@ -17,32 +17,20 @@ export function BuyNow(props: {
     console.log("BuyNow redirectToCheckout");
     try {
       setIsLoading(true);
-      const createdCart = await cart.createCart({
-        lineItems: [
-          {
-            catalogReference: {
-              catalogItemId: props.productId,
-              appId: "1380b703-ce81-ff05-f115-39571d94dfcd",
-              options: {
-                  options: props.variant,
-              }
-            },
-            quantity: 1
+
+      const { checkoutId } = await checkout.createCheckout({
+        lineItems: [{
+          catalogReference: {
+            catalogItemId: props.productId,
+            appId: "1380b703-ce81-ff05-f115-39571d94dfcd",
+            options: {
+                options: props.variant,
+            }
           },
-        ],
+          quantity: 1
+        }],
+        channelType: checkout.ChannelType.WEB,
       });
-
-      if (!createdCart || !createdCart._id) {
-        throw new Error("Failed to create cart :(");
-      }
-
-      console.log(`Buy now cart created: ${createdCart._id}`);
-
-      const { checkoutId } = await cart.createCheckout(createdCart._id, {
-        channelType: cart.ChannelType.WEB
-      });
-
-      console.log(`Buy now checkout created: ${checkoutId}`);
 
       const { redirectSession } = await redirects.createRedirectSession({
         ecomCheckout: { checkoutId },
@@ -51,10 +39,7 @@ export function BuyNow(props: {
         },
       });
 
-      console.log(`Buy now redirecting to: ${redirectSession?.fullUrl}`);
       window.location.href = redirectSession?.fullUrl!;
-    } catch (error) {
-      console.error("Error during checkout:", error);
     } finally {
       setIsLoading(false);
     }
