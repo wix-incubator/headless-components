0e653f79c3f1186c1301dd1f6feef4f347696784
Yury Michurin <yurym@wix.com>
2025-05-29 10:10:23 +0300

    Sprinkle some promises

--- packages/headless-components/core/src/directive.ts
+++ packages/headless-components/core/src/directive.ts
@@ -1,17 +1,87 @@
 import type { ClientDirective } from "astro";
 
-export default <ClientDirective>(async (load, _, el) => {
-  await new Promise(resolve => setTimeout(resolve, 5000));
+function createPromiseHandle<T>() : {
+  promise: Promise<T>;
+  resolve: (value: T) => void;
+  reject: (reason?: unknown) => void;
+} {
+  let resolve: (value: T) => void;
+  let reject: (reason?: unknown) => void;
+  const promise = new Promise<T>((res, rej) => {
+    resolve = res;
+    reject = rej;
+  });
+  return { promise, resolve: resolve!, reject: reject! };
+}
 
-  const service = (el.closest("context-provider") as any).service;
-  console.log("context-provider service", service);
+type Task = {
+  load: () => Promise<(props?: any) => Promise<void>>;
+  el: Element;
+  promiseHandle: ReturnType<typeof createPromiseHandle<void>>;
+}
+
+const pendingTasks: Set<Task> = new Set();
+
+export default <ClientDirective>(async (load, _, el) => {
+  const task: Task = {
+    load,
+    el,
+    promiseHandle: createPromiseHandle<void>(),
+  };
+  pendingTasks.add(task);
+
+  await processPendingTasks();
+
+  return task.promiseHandle.promise;
+});
+
+async function processPendingTasks() {
+  const tasks = Array.from(pendingTasks);
+  for (const task of tasks) {
+    await processTask(task);
+  }
+}
+
+async function processTask(task: Task) {
+  const service = getServiceFromElement(task.el);
+  if (!service) {
+    console.log("Service not found on closest service provider");
+    return;
+  }
+  assignServiceToElement(task.el, service);
+  const [hydrate] = await Promise.all([task.load()]);
+  await hydrate();
+  task.promiseHandle.resolve();
+  pendingTasks.delete(task);
+}
+
+function assignServiceToElement(el: Element, service: any) {
+  console.log("assign service to element", service);
   const contextId = crypto.randomUUID();
   (globalThis as any).contexts = (globalThis as any).contexts || {};
   (globalThis as any).contexts[contextId] = service;
+  setContextIdOnProps(el, contextId);
+}
+
+
+function getServiceFromElement(el: Element) {
+  const ctxProvider = el.closest("context-provider");
+  if (!ctxProvider) {
+    return null;
+  }
+  const service = (ctxProvider as any).service;
+  if (!service) {
+    return null;
+  }
+  return service;
+}
+
+function setContextIdOnProps(el: Element, contextId: string) {
   const props = JSON.parse(el.getAttribute("props") || "{}");
   console.log("context-provider Props from wait-for-store:", props);
+
   props["contextId"] = [0, contextId];
   el.setAttribute("props", JSON.stringify(props));
-  const [hydrate] = await Promise.all([load()]);
-  await hydrate();
-});
+}
+
+(globalThis as any).runDirectivePendingTaks = processPendingTasks;
