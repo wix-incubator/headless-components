9aeef572e4f5a94b7c033b66fff1dc63a0aa0e04
Yury Michurin <yurym@wix.com>
2025-06-09 16:01:24 +0300

    Cleanup

--- examples/astro-components-demo/src/actions/custom-checkout.ts
+++ examples/astro-components-demo/src/actions/custom-checkout.ts
@@ -1,11 +1,10 @@
 import { defineAction } from 'astro:actions';
-import { getCustomLineItemCheckoutURLFactory, type CustomLineItemCheckoutOptions } from '@wix/headless-stores/server-actions';
+import { getCustomLineItemCheckoutURLFactory } from '@wix/headless-stores/server-actions';
 
 const customCheckout = getCustomLineItemCheckoutURLFactory({ price: "100" });
 
 export const customCheckoutAction = defineAction({
   handler: async function() {
-    console.log("customCheckoutAction started");
     const checkoutUrl = await customCheckout({
       productName: "test",
       priceDescription: "test",
@@ -13,7 +12,6 @@ export const customCheckoutAction = defineAction({
       quantity: 1,
     });
 
-    console.log("customCheckoutAction ended");
     return checkoutUrl;
   },
 });
