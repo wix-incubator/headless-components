674fb05be2f7ed92d986c8dfdf4bbde0f198f4fb
nitayneeman <7141972+nitayneeman@users.noreply.github.com>
2025-07-15 10:00:02 +0300

    remove

--- packages/@wix-astro/src/auth-context.ts
+++
@@ -1,6 +0,0 @@
-import { AsyncLocalStorage } from "node:async_hooks";
-import type { AuthenticationStrategy } from "@wix/sdk";
-
-export const authStrategyAsyncLocalStorage = new AsyncLocalStorage<{
-  auth: AuthenticationStrategy<void>;
-}>();
--- packages/@wix-astro/src/routes/auth/runtime.ts
+++
@@ -1,9 +0,0 @@
-import { Tokens } from "@wix/sdk";
-import { WIX_CLIENT_ID } from "astro:env/client";
-
-export function sessionCookieJson(tokens: Tokens) {
-  return {
-    clientId: WIX_CLIENT_ID,
-    tokens,
-  };
-}
