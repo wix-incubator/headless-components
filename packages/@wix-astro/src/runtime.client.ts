53c0b14da94a064189810605ec834d8362e08339
Nitay Neeman <nitayneeman@users.noreply.github.com>
2025-07-15 10:19:52 +0300

    refactor: simplify build process and clean up repository (#67)

--- packages/@wix-astro/src/runtime.client.ts
+++
@@ -1,6 +0,0 @@
-import { wixContext } from '@wix/sdk-context';
-import type { SiteSessionAuth } from "@wix/sdk/auth/site-session";
-
-export function getAuth() : ReturnType<typeof SiteSessionAuth> {
-  return (wixContext['client'] as any).auth;
-}
