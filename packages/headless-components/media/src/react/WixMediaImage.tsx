9363b6137830bbe6a4a57298cc863a8f516d636b
Yury Michurin <yurym@wix.com>
2025-08-13 09:59:44 +0300

    Get back @wix/image dep for now

--- packages/headless-components/media/src/react/WixMediaImage.tsx
+++ packages/headless-components/media/src/react/WixMediaImage.tsx
@@ -1,24 +1,30 @@
 import React from "react";
 import { media as wixMedia } from "@wix/sdk";
+import { Image, type FittingType, initCustomElement } from '@wix/image'; // Note: this import SCSS
 import { Slot } from "@radix-ui/react-slot";
+
+initCustomElement();
 
 type MediaItem = { image?: string };
 
 export interface WixMediaImageProps extends React.ImgHTMLAttributes<HTMLImageElement> {
   media?: MediaItem;
   asChild?: boolean;
+  displayMode?: FittingType;
+  showPlaceholder?: boolean;
 }
+
 
 export const WixMediaImage = React.forwardRef<
   HTMLImageElement,
   WixMediaImageProps
->(({ media, width, height, className, alt = "", asChild, children, ...rest }, ref) => {
+>(({ media, width, height, className, alt = "", asChild, children, displayMode = 'fill', showPlaceholder = true, ...rest }, ref) => {
   if (!media?.image) return null;
 
   const parsed = wixMedia.getImageUrl(media.image);
   const src = parsed.url;
-  const derivedWidth = width || parsed.width;
-  const derivedHeight = height || parsed.height;
+  const derivedWidth = Number(width) || parsed.width;
+  const derivedHeight = Number(height) || parsed.height;
   const derivedAlt = parsed.altText ?? alt;
 
   if (asChild) {
@@ -31,6 +37,7 @@ export const WixMediaImage = React.forwardRef<
         height={derivedHeight}
         alt={derivedAlt}
         className={className}
+        ref={ref}
         {...rest}
       >
         {children}
@@ -39,14 +46,18 @@ export const WixMediaImage = React.forwardRef<
   }
 
   return (
-    <img
-      ref={ref}
-      src={src}
+    <Image
+      key={src}
+      uri={src}
       width={derivedWidth}
       height={derivedHeight}
-      alt={derivedAlt}
+      containerWidth={derivedWidth}
+      containerHeight={derivedHeight}
+      displayMode={displayMode}
+      isSEOBot={false}
+      shouldUseLQIP={showPlaceholder}
+      alt={alt}
       className={className}
-      {...rest}
     />
   );
 });
