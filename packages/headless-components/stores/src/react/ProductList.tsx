0c7a2b0158b4cf9f3c33867b0a6e19b5a11969ea
nitayneeman <7141972+nitayneeman@users.noreply.github.com>
2025-08-20 11:47:55 +0300

    implement

--- packages/headless-components/stores/src/react/ProductList.tsx
+++ packages/headless-components/stores/src/react/ProductList.tsx
@@ -8,12 +8,14 @@ import { ProductsListServiceDefinition } from '../services/products-list-service
 import * as CoreProductList from './core/ProductList.js';
 import * as CoreProductListPagination from './core/ProductListPagination.js';
 import * as Product from './Product.js';
+import { type AsChildProps, renderAsChild } from '../utils/renderAsChild.js';
 
 enum TestIds {
   productListRoot = 'product-list-root',
   productListProducts = 'product-list-products',
   productListItem = 'product-list-item',
   productListLoadMore = 'product-list-load-more',
+  productListTotalsDisplayed = 'product-list-totals-displayed',
 }
 
 /**
@@ -331,5 +333,61 @@ export const LoadMoreTrigger = React.forwardRef<
         );
       }}
     </CoreProductListPagination.LoadMoreTrigger>
+  );
+});
+
+/**
+ * Props for ProductList Totals Displayed component
+ */
+export interface TotalsDisplayedProps
+  extends AsChildProps<{ displayedProducts: number }> {}
+
+/**
+ * Displays the number of products currently displayed.
+ *
+ * @component
+ * @example
+ * ```tsx
+ * <ProductList.TotalsDisplayed />
+ * // or with asChild
+ * <ProductList.TotalsDisplayed asChild>
+ *   <strong />
+ * </ProductList.TotalsDisplayed>
+ * // or with render function
+ * <ProductList.TotalsDisplayed asChild>
+ *   {({ displayedProducts }, ref) => <strong ref={ref}>{displayedProducts}</strong>}
+ * </ProductList.TotalsDisplayed>
+ * ```
+ */
+export const TotalsDisplayed = React.forwardRef<
+  HTMLElement,
+  TotalsDisplayedProps
+>((props, ref) => {
+  const { asChild, children, className } = props;
+  const productsListService = useService(ProductsListServiceDefinition);
+  const products = productsListService.products.get();
+  const displayedProducts = products.length;
+
+  const attributes = {
+    'data-testid': TestIds.productListTotalsDisplayed,
+    'data-displayed': displayedProducts,
+    className,
+  };
+
+  if (asChild) {
+    const rendered = renderAsChild({
+      children,
+      props: { displayedProducts },
+      ref,
+      content: displayedProducts.toString(),
+      attributes,
+    });
+    if (rendered) return rendered;
+  }
+
+  return (
+    <span {...attributes} ref={ref as React.Ref<HTMLSpanElement>}>
+      {displayedProducts}
+    </span>
   );
 });
