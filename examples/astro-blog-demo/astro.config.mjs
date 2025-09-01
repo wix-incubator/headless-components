d32fc3a01d8e9aafee66a79c8b644cdd33055788
Žanas Stundys <zanass@wix.com>
2025-09-01 12:26:41 +0300

    Setup https logic for astro

--- examples/astro-blog-demo/astro.config.mjs
+++ examples/astro-blog-demo/astro.config.mjs
@@ -3,12 +3,28 @@ import react from '@astrojs/react';
 import wix from '@wix/astro';
 import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
+import fs from 'fs';
+
+const hasCertificates = fs.existsSync('./localhost+2-key.pem');
 
 // https://astro.build/config
 export default defineConfig({
-  site: process.env.WIX_PUBLIC_URL || 'https://example.com',
-  integrations: [react(), wix({ enableHtmlEmbeds: false }), sitemap()],
+  site: process.env.WIX_PUBLIC_URL || 'http://localhost:4321/',
+  integrations: [
+    react(),
+    wix({ enableAuthRoutes: true, enableHtmlEmbeds: false }),
+    sitemap(),
+  ],
   vite: {
+    server: {
+      https: hasCertificates
+        ? {
+            key: './localhost+2-key.pem',
+            cert: './localhost+2.pem',
+          }
+        : undefined,
+      host: true,
+    },
     plugins: [tailwindcss()],
   },
 });
