baf8908b096ab3551347155a2eee3c3d7444dc5f
jordanlaor <jordanl@wix.com>
2025-09-07 10:32:39 +0300

    chore(cms): move existing code to legacy/ to prep for the new implementation

--- packages/headless-components/cms/src/services/index.ts
+++ packages/headless-components/cms/src/services/index.ts
@@ -1,11 +1,3 @@
 /**
  * @file Services index file for CMS package
  */
-
-export {
-  CMSServiceDefinition,
-  CMSServiceImplementation,
-  loadCMSServiceInitialData,
-  CMSServiceBinding,
-  type CMSServiceConfig,
-} from './cms-crud-service.js';
