2a7c5e62260f0aed9be7e6015f01123f5b5d735d
yehuda23 <yehudaba@wix.com>
2025-09-09 14:27:20 +0300

    add rules and move old code to legacy

---
+++ packages/headless-components/cms/src/legacy/services/index.ts
@@ -0,0 +1,11 @@
+/**
+ * @file Services index file for CMS package
+ */
+
+export {
+  CMSServiceDefinition,
+  CMSServiceImplementation,
+  loadCMSServiceInitialData,
+  CMSServiceBinding,
+  type CMSServiceConfig,
+} from './cms-crud-service.js';
--- packages/headless-components/cms/src/react/index.tsx
+++ packages/headless-components/cms/src/react/index.tsx
@@ -2,7 +2,4 @@
  * @file React components index file for CMS package
  */
 
-export * as CMSCrud from './CMSCrud.js';
-export * as CMSPagination from './CmsCrudPagination.js';
-export * as CMSSort from './CmsCrudSort.js';
-export * as CMSFilter from './CMSFilter.js';
+export {}
