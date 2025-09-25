686b2f04b77ac6aad4139dae7474538a8f6e59f5
hbmoshe <53140677+hbmoshe@users.noreply.github.com>
2025-09-25 17:14:01 +0300

    remove unused code

--- packages/headless-components/instagram/src/react/InstagramFeed.tsx
+++ packages/headless-components/instagram/src/react/InstagramFeed.tsx
@@ -6,7 +6,6 @@ export { Title } from './Title.js';
 export { UserName } from './UserName.js';
 export { Hashtag } from './Hashtag.js';
 export { InstagramMedias } from './InstagramMedias.js';
-export { InstagramMediaItems } from './InstagramMediaItems.js';
 export { InstagramMediaRepeater } from './InstagramMediaRepeater.js';
 export { InstagramMedia } from './InstagramMedia.js';
 
@@ -16,5 +15,4 @@ export type { TitleProps } from './Title.js';
 export type { UserNameProps } from './UserName.js';
 export type { HashtagProps } from './Hashtag.js';
 export type { InstagramMediasProps } from './InstagramMedias.js';
-export type { InstagramMediaItemsProps } from './InstagramMediaItems.js';
 export type { InstagramMediaRepeaterProps } from './InstagramMediaRepeater.js';
