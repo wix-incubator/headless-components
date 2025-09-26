00e0a6c93bd3738f59d1c6ac53f700372744c98a
dovydasm <dovydasm@wix.com>
2025-09-26 13:37:50 +0300

    WIP

--- packages/headless-components/events/src/utils/date.ts
+++ packages/headless-components/events/src/utils/date.ts
@@ -1,12 +1,12 @@
-export const formatShortDate = (date: Date) =>
-  date.toLocaleDateString('en-US', {
+export const formatShortDate = (date: string | Date) =>
+  new Date(date).toLocaleDateString('en-US', {
     weekday: 'short',
     day: '2-digit',
     month: 'short',
   });
 
-export const formatFullDate = (date: Date) =>
-  date.toLocaleDateString('en-US', {
+export const formatFullDate = (date: string | Date) =>
+  new Date(date).toLocaleDateString('en-US', {
     year: 'numeric',
     month: 'short',
     day: 'numeric',
@@ -14,14 +14,21 @@ export const formatFullDate = (date: Date) =>
     minute: '2-digit',
   });
 
-export const formatTime = (date: Date) =>
-  date.toLocaleTimeString('en-US', {
+export const formatTime = (date: string | Date) =>
+  new Date(date).toLocaleTimeString('en-US', {
     hour: '2-digit',
     minute: '2-digit',
   });
 
-export const formatTimeRange = (startTime: Date, endTime: Date) =>
-  `${formatTime(startTime)} - ${formatTime(endTime)}`;
+export const formatTimeRange = (
+  startTime: string | Date,
+  endTime: string | Date,
+) => `${formatTime(startTime)} - ${formatTime(endTime)}`;
 
-export const getDurationInMinutes = (startTime: Date, endTime: Date) =>
-  Math.round((endTime.getTime() - startTime.getTime()) / (1000 * 60));
+export const getDurationInMinutes = (
+  startTime: string | Date,
+  endTime: string | Date,
+) =>
+  Math.round(
+    (new Date(endTime).getTime() - new Date(startTime).getTime()) / (1000 * 60),
+  );
