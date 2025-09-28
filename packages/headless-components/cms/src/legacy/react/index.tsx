8513de40ad5c186be387b5a7482df45dff408693
jordanlaor <jordanl@wix.com>
2025-09-28 11:08:28 +0300

    Remove legacy CMS components and services, including CRUD, pagination, sorting, and filtering functionalities, along with their associated tests and index files.

--- packages/headless-components/cms/src/legacy/react/index.tsx
+++
@@ -1,8 +0,0 @@
-/**
- * @file React components index file for CMS package
- */
-
-export * as CMSCrud from './CMSCrud.js';
-export * as CMSPagination from './CmsCrudPagination.js';
-export * as CMSSort from './CmsCrudSort.js';
-export * as CMSFilter from './CMSFilter.js';
