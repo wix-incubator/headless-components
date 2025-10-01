1f252149333346606315dcadb699f16473afa693
nivinab <107550239+nivinab@users.noreply.github.com>
2025-10-01 14:42:47 +0300

    Fix comments to Instagram headless components  (#365)
    * fix comments
    
    * remove comment

--- packages/headless-components/instagram/src/react/InstagramFeed.tsx
+++ packages/headless-components/instagram/src/react/InstagramFeed.tsx
@@ -2,15 +2,11 @@
 // This file creates the Instagram namespace with all components
 
 export { Root } from './InstagramFeedRoot.js';
-export { Title } from './Title.js';
 export { UserName } from './UserName.js';
-export { Hashtag } from './Hashtag.js';
 export { InstagramMedias } from './InstagramMedias.js';
 export { InstagramMedia } from './InstagramMedia.js';
 
 // Export component prop types
 export type { RootProps } from './InstagramFeedRoot.js';
-export type { TitleProps } from './Title.js';
 export type { UserNameProps } from './UserName.js';
-export type { HashtagProps } from './Hashtag.js';
 export type { InstagramMediasProps } from './InstagramMedias.js';
