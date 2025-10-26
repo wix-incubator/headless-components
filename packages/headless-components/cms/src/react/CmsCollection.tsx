a003caf90750b313b0344a1550985d0e8b797d32
yehuda23 <yehudaba@wix.com>
2025-10-26 09:36:26 +0200

    [CMS] update create button props type and expose error

--- packages/headless-components/cms/src/react/CmsCollection.tsx
+++ packages/headless-components/cms/src/react/CmsCollection.tsx
@@ -810,7 +810,8 @@ export interface CreateItemActionProps {
     | AsChildChildren<{
         disabled: boolean;
         isLoading: boolean;
-        onClick: () => void;
+        error: string | null;
+        insertItemOrReference: (params: InsertItemOrReferenceParams) => Promise<WixDataItem | void>;
       }>
     | React.ReactNode;
   /** CSS classes to apply to the default element */
@@ -931,7 +932,7 @@ export const CreateItemAction = React.forwardRef<
 
   return (
     <CoreCmsCollection.CreateItemAction>
-      {({ insertItemOrReference, isLoading }) => {
+      {({ insertItemOrReference, isLoading, error }) => {
         const disabled = isLoading;
 
         // Build params based on provided props
@@ -963,6 +964,7 @@ export const CreateItemAction = React.forwardRef<
             customElementProps={{
               disabled,
               isLoading,
+              error,
               insertItemOrReference: (params: InsertItemOrReferenceParams) =>
                 insertItemOrReference(params),
             }}
