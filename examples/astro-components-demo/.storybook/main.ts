b38bf2a7e59a0a7c3f32c9f9e71a2f61cd7deb03
arielWix <arielh@wix.com>
2025-11-11 12:08:16 +0200

    adding sort storybook #pr

---
+++ examples/astro-components-demo/.storybook/main.ts
@@ -0,0 +1,27 @@
+import type { StorybookConfig } from '@storybook/react-vite';
+import { mergeConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
+
+const config: StorybookConfig = {
+  stories: ['../src/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+  ],
+  framework: {
+    name: '@storybook/react-vite',
+    options: {},
+  },
+  docs: {
+    autodocs: 'tag',
+  },
+  async viteFinal(config) {
+    return mergeConfig(config, {
+      plugins: [tailwindcss()],
+    });
+  },
+};
+
+export default config;
+
